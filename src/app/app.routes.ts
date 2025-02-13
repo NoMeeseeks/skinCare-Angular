522b1826955a5e1228289a13e93696286d03bfa9
Josie <joselinevergara@uees.edu.ec>
2025-02-13 13:00:04 -0500

    Avance Home Page
    Se estructuro el diseño de la pagina, falta mejorarla

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
 import {MainLayoutComponent} from './layout/main-layout/main-layout.component';
+import { HomePageComponent } from './modules/home-page/home-page.component';
 
 export const routes: Routes = [
 
   {
     path:'',
     component: MainLayoutComponent,
+    children:[
+      {
+        path:'HomePage',
+        component: HomePageComponent
+      },
+    ]
   },
 ];
