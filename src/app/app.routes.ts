8f2671aad95113212b9f03b0f73c56f14698324f
melanie <melanieleon20@hotmail.com>
2025-02-14 19:19:45 -0500

    se avanzo con la pagina about us

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {MainLayoutComponent} from './layout/main-layout/main-layout.component';
 import { HomePageComponent } from './modules/home-page/home-page.component';
 import { RegistroComponent } from './modules/registro/registro.component';
+import { AboutUsComponent } from './modules/about-us/about-us.component';
 
 export const routes: Routes = [
 
@@ -13,6 +14,10 @@ export const routes: Routes = [
         path:'HomePage',
         component: HomePageComponent
       },
+    {
+      path:'AboutUs',
+      component: AboutUsComponent
+    }
     ]
   },
   {
