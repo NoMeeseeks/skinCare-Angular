46e1f9084a7280a19c6c783976379b1e53a9aa77
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2025-02-17 07:45:36 -0500

    Fix navBar a component

--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../footer/footer.component';
+import {NavBarComponent} from '../nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, FooterComponent,RouterLink],
+  imports: [RouterOutlet, FooterComponent, RouterLink, NavBarComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
